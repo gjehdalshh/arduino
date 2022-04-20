2d212c4f83f7cf9a60958fc63ef0956c943c1214
sooye <sooye@DESKTOP-MBD9PAR>
2022-04-20 11:31:01 +0900

    [Design]"추가 practiceMode.jsp, 변경 home.jsp"  practiceMode.jsp와 home.jsp, home.css 디자인 추가 및 변경

---
+++ arduino/src/main/java/com/arduino/join/JoinMapper.java
@@ -0,0 +1,8 @@
+package com.arduino.join;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JoinMapper {
+
+}
---
+++ arduino/src/main/java/com/arduino/join/JoinService.java
@@ -0,0 +1,8 @@
+package com.arduino.join;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+}
