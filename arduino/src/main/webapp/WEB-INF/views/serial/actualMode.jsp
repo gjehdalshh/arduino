8a944407e874a19cd5f616d2e68714eed7f67e6d
동민 허 <gjehdalshh@naver.com>
2022-05-09 21:51:50 +0900

    [Feat] Timer function animation-play-state

--- arduino/src/main/webapp/WEB-INF/views/serial/actualMode.jsp
+++ arduino/src/main/webapp/WEB-INF/views/serial/actualMode.jsp
@@ -10,15 +10,27 @@
 		</div>
 		<div class="countdown"></div>
 	</div>
+	<!-- 설정 모달창 -->
+	<div onclick="setting()">설정</div>
+	
+	<div class="setBlack_bg"></div>
+	<div class="setModal_wrap">
+		<div class="restart">시작하기</div>
+		<div class="setModal_close">
+			<div>종료하기</div>
+		</div>
+	</div>
+	
 
+	<!-- 초기화면 모달창 -->
 	<div class="black_bg"></div>
 	<div class="modal_wrap">
 		<div>옥타브 설정</div>
 		<div class="setOctave">
-			<div class="octave">3옥타브</div>
-			<div class="octave">4옥타브</div>
-			<div class="octave">5옥타브</div>
-			<div class="octave">6옥타브</div>
+			<div id="oc3" class="octave" onclick="getOctaveValue(0)">3옥타브</div>
+			<div id="oc4" class="octave" onclick="getOctaveValue(1)">4옥타브</div>
+			<div id="oc5" class="octave" onclick="getOctaveValue(2)">5옥타브</div>
+			<div id="oc6" class="octave" onclick="getOctaveValue(3)">6옥타브</div>
 		</div>
 		<div class="modal_close">
 			<div>시작하기</div>
@@ -26,5 +38,5 @@
 	</div>
 </div>
 
-<script defer src="/res/js/serial/actualMode.js?ver=95"></script>
+<script defer src="/res/js/serial/actualMode.js?ver=107"></script>
 
--- arduino/src/main/webapp/resources/js/serial/actualMode.js
+++ arduino/src/main/webapp/resources/js/serial/actualMode.js
@@ -4,6 +4,7 @@ let defaultTime = document.querySelector('.defaultTime')
 let countdown = document.querySelector('.countdown')
 let count = defaultTime.value
 let val = 0
+let interval = 0
 
 function startActualMode() {
 	if (val == 0) {
@@ -20,6 +21,8 @@ function myTimer() {
 
 	countdown.innerHTML = `<div>${count}</div>`
 	count = count - 1
+	timer.style = "animation-play-state: running"
+	mask.style = "animation-play-state: running"
 
 	if (count == 0) {
 		count = 5
@@ -27,14 +30,15 @@ function myTimer() {
 }
 
 function setTime() {
-	setInterval(myTimer, 1000)
+	interval = setInterval(myTimer, 1000)
+	console.log(interval)
 }
 
 window.onload = function() {
-	
+
 	document.querySelector('.modal_wrap').style.display = 'block'
 	document.querySelector('.black_bg').style.display = 'block'
-	
+
 	function offClick() {
 		document.querySelector('.modal_wrap').style.display = 'none'
 		document.querySelector('.black_bg').style.display = 'none'
@@ -43,5 +47,50 @@ window.onload = function() {
 
 	document.querySelector('.modal_close').addEventListener('click', offClick)
 }
+
+function setting() {
+	clearInterval(interval)
+
+	timer.style = "animation-play-state: paused"
+	mask.style = "animation-play-state: paused"
+
+	document.querySelector('.setModal_wrap').style.display = 'block'
+	document.querySelector('.setBlack_bg').style.display = 'block'
+
+	function moveHome() {
+		location.href = `/main/home`
+	}
+	function restart() {
+		document.querySelector('.setModal_wrap').style.display = 'none'
+		document.querySelector('.setBlack_bg').style.display = 'none'
+		interval = setInterval(myTimer, 1000)
+	}
+
+	document.querySelector('.restart').addEventListener('click', restart)
+	document.querySelector('.setModal_close').addEventListener('click', moveHome)
+}
+
+let oc3 = document.querySelector('#oc3')
+let oc4 = document.querySelector('#oc4')
+let oc5 = document.querySelector('#oc5')
+let oc6 = document.querySelector('#oc6')
+
+function getOctaveValue(value) {
+	oc3.style.backgroundColor = 'white'
+	oc4.style.backgroundColor = 'white'
+	oc5.style.backgroundColor = 'white'
+	oc6.style.backgroundColor = 'white'
+
+	if (value == 0) {
+		oc3.style.backgroundColor = 'skyblue'
+	} else if (value == 1) {
+		oc4.style.backgroundColor = 'skyblue'
+	} else if (value == 2) {
+		oc5.style.backgroundColor = 'skyblue'
+	} else if (value == 3) {
+		oc6.style.backgroundColor = 'skyblue'
+	}
+}
+
 
 
